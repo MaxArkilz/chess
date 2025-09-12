058add9d28ae7b5f2771df817245fdb2b5ad9ab1
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-09-11 22:48:41 -0600

    helper functions added

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -254,4 +254,51 @@ public class ChessPiece {
             moves.add(new ChessMove(myPosition, newPos, null));
         }
     }
+
+    private void slidingMoves (ChessBoard board, ChessPosition start, List<ChessMove> moves, int[][] directions) {
+        for (int[] dir:directions) {
+            int row = start.getRow() + dir[0];
+            int col = start.getColumn() + dir[1];
+
+            while (insideBoard(row,col)) {
+                ChessPosition newPos = new ChessPosition(row,col);
+                ChessPiece target = board.getPiece(newPos);
+
+                if (target == null) {
+                    moves.add(new ChessMove(start, newPos, null));
+                } else {
+                    if (target.getTeamColor() != this.getTeamColor()) {
+                        moves.add(new ChessMove(start, newPos,null));
+                    }
+                    break;
+                }
+                row += dir[0];
+                col += dir[1];
+            }
+        }
+    }
+
+    private  void stepMoves (ChessBoard board, ChessPosition start, List<ChessMove> moves, int[][] directions) {
+        for (int[] dir:directions) {
+            int row = start.getRow() + dir[0];
+            int col = start.getColumn() + dir[1];
+
+            if (insideBoard(row, col)) {
+                ChessPosition newPos = new ChessPosition(row, col);
+                ChessPiece target = board.getPiece(newPos);
+
+                if (target == null || target.getTeamColor() != this.getTeamColor()) {
+                    moves.add(new ChessMove(start,newPos, null));
+                }
+            }
+        }
+    }
+
+    private void pawnMoves (ChessBoard board, ChessPosition start, List<ChessMove> moves){
+
+    }
+
+    private boolean insideBoard(int row, int col) {
+        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+    }
 }
