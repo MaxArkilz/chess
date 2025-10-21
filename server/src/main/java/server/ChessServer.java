0f43ca16801fb900363e864711c03f48c8f683f0
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-10-21 16:17:07 -0600

    login handler

--- server/src/main/java/server/ChessServer.java
+++ server/src/main/java/server/ChessServer.java
@@ -6,25 +6,29 @@ import io.javalin.*;
 import io.javalin.http.Context;
 import model.UserData;
 import passoff.exception.ResponseParseException;
+import service.GameService;
 import service.UserService;
 
 public class ChessServer {
 
     private final Javalin httpHandler;
-    private final UserService service;
+    private final UserService userService;
+    private final GameService gameService;
 
-    public ChessServer(UserService service) {
-        this.service = service;
+    public ChessServer(UserService userService, GameService gameService) {
+        this.userService = userService;
+        this.gameService = gameService;
 
         // Register your endpoints and exception handlers here.
         this.httpHandler = Javalin.create(config -> config.staticFiles.add("public"))
                 .post("/session", this::login)
-                .delete("/db", this::clearApplication)
-                .post("/user", this::register)
                 .delete("/session", this::logout)
+                .post("/user", this::register)
                 .get("/game", this::listGames)
                 .post("/game", this::createGame)
-                .put("/game", this::joinGame);
+                .put("/game", this::joinGame)
+                .delete("/db", this::clearApplication)
+                .exception(ResponseParseException.class, this::exeptionHandler);
 
     }
 
@@ -37,10 +41,14 @@ public class ChessServer {
         httpHandler.stop();
     }
 
+    private void exceptionHandler(ResponseParseException ex, Context ctx) {
+        ctx.status(ex.toHttpStatusCode());
+        ctx.json(ex.toJson());
+    }
 
     private void login(Context ctx) throws ResponseParseException {
         UserData.LoginRequest request = new Gson().fromJson(ctx.body(), UserData.LoginRequest.class);
-        UserData.LoginResult result = service.login(request);
+        UserData.LoginResult result = userService.login(request);
     }
 
     private void clearApplication(Context ctx) {
