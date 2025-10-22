02723909f1390cffbf5c249e50fc83454f6b1d91
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-10-21 19:16:09 -0600

    imighthaveasmallideawhatimdoing

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,14 +1,19 @@
 package service;
 
 import model.UserData;
+import exception.ResponseException;
 
 public class UserService {
-    public UserData register(UserData.RegisterRequest registerRequest) {
-        return null;
+    public UserData.RegisterRequest register(UserData.RegisterRequest registerRequest)throws ResponseException{
+        if (registerRequest.username() != null) {
+            throw new ResponseException(403);
+        }
     }
+
     public UserData login(UserData.LoginRequest loginRequest){
         return null;
     }
+
     public void logout(String logoutRequest) {}
 
 }
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,10 +1,23 @@
 package model;
+import java.util.UUID;
 
-public record UserData() {
+public record UserData(String username, String password, String email) {
+
+    public static LoginRequest login(String username, String password) {
+        return new LoginRequest(username, password);
+    }
+
+    public static LoginResult result(String username,String authToken){
+        return new LoginResult(username, UUID.randomUUID().toString());
+    }
+
+    public static RegisterRequest register(String username, String password, String email){
+        return new RegisterRequest(username, password, email);
+    }
+
     public record LoginRequest(String username, String password) {}
+    public record RegisterRequest(String username, String password, String email) {}
+    public record LoginResult(String username, String authToken) {}
 
-    public record LoginResult(String username,String authToken){}
-
-    public record RegisterRequest(String username, String password, String email){}
 }
 
