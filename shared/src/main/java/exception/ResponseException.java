6593e7da738352f13a66e5f345fd1481e805cdcd
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-10-21 19:28:15 -0600

    ResponseException.java simplification

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,51 +1,29 @@
 package exception;
 
-import com.google.gson.Gson;
+public class ResponseException extends RuntimeException{
+    private final int statusCode;
+    private final String message;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class ResponseException extends Exception {
-
-    public enum Code {
-        ServerError,
-        ClientError,
+    public ResponseException(int statusCode, String message) {
+        super("Error " + statusCode + ": " + message);
+        this.statusCode = statusCode;
+        this.message = message;
     }
 
-    final private Code code;
-
-    public ResponseException(Code code, String message) {
-        super(message);
-        this.code = code;
+    public int getStatusCode() {
+        return statusCode;
     }
 
-    public String toJson() {
-        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    @Override
+    public String getMessage(){
+        return message;
     }
 
-    public static ResponseException fromJson(String json) {
-        var map = new Gson().fromJson(json, HashMap.class);
-        var status = Code.valueOf(map.get("status").toString());
-        String message = map.get("message").toString();
-        return new ResponseException(status, message);
-    }
-
-    public Code code() {
-        return code;
-    }
-
-    public static Code fromHttpStatusCode(int httpStatusCode) {
-        return switch (httpStatusCode) {
-            case 500 -> Code.ServerError;
-            case 400 -> Code.ClientError;
-            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
-        };
-    }
-
-    public int toHttpStatusCode() {
-        return switch (code) {
-            case ServerError -> 500;
-            case ClientError -> 400;
-        };
+    @Override
+    public String toString() {
+        return "ResponseException{" +
+                "statusCode=" + statusCode +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
