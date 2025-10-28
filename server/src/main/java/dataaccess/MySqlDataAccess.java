02442fd3aee92ac7910b6756afa782ec5356ca18
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-10-28 16:49:50 -0600

    MySql schema creation

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,40 @@
+package dataaccess;
+
+import exception.ResponseException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MySqlDataAccess {
+
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS game(
+            'id' int NOT NULL AUTO_INCREMENT,
+            'name' varchar(256) NOT NULL,
+            'playerColor' ENUM('WHITE', 'BLACK') DEFAULT 'WHITE',
+            'json' TEXT DEFAULT NULL,
+            PRIMARY KEY ('id'),
+            INDEX(name)
+            )
+"""
+    };
+    
+    private void configureDatabase() throws DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)){
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(
+                    500,
+                    String.format("Unable to configure database: %s", ex.getMessage()),
+                    ResponseException.Code.ServerError);
+        }
+
+    }
+}
--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -1,17 +1,61 @@
 package exception;
 
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResponseException extends RuntimeException{
     private final int statusCode;
     private final String message;
 
-    public ResponseException(int statusCode, String message) {
+
+    public enum Code {
+        ServerError,
+        ClientError,
+    }
+
+    final private Code code;
+
+    public ResponseException(int statusCode, String message, Code code) {
         super("Error " + statusCode + ": " + message);
         this.statusCode = statusCode;
         this.message = message;
+        this.code = code;
     }
 
     public int getStatusCode() {
         return statusCode;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", code));
+    }
+
+    public static ResponseException fromJson(String json) {
+        var map = new Gson().fromJson(json, HashMap.class);
+        var status = Code.valueOf(map.get("status").toString());
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
+
+    public Code code() {
+        return code;
+    }
+
+    public static Code fromHttpStatusCode(int httpStatusCode) {
+        return switch (httpStatusCode) {
+            case 500 -> Code.ServerError;
+            case 400 -> Code.ClientError;
+            default -> throw new IllegalArgumentException("Unknown HTTP status code: " + httpStatusCode);
+        };
+    }
+
+    public int toHttpStatusCode() {
+        return switch (code) {
+            case ServerError -> 500;
+            case ClientError -> 400;
+        };
     }
 
     @Override
