839fc0eda93373ba757ee46e0da8106a330ef95a
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-12-01 23:14:44 -0700

    basic facade setup

---
+++ client/src/main/java/websocketClient/WebSocketFacade.java
@@ -0,0 +1,38 @@
+package websocketClient;
+import com.google.gson.Gson;
+import exception.ResponseException;
+import websocket.*;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http","ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                public void onMessage(String message) {
+                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {}
+}
