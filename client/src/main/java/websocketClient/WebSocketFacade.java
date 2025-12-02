050272b73b5b80ac121595302e9d8dc6c12217af
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-12-01 23:33:36 -0700

    connect endpoint initial function

--- client/src/main/java/websocketClient/WebSocketFacade.java
+++ client/src/main/java/websocketClient/WebSocketFacade.java
@@ -2,6 +2,7 @@ package websocketClient;
 import com.google.gson.Gson;
 import exception.ResponseException;
 import websocket.*;
+import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 import javax.websocket.*;
@@ -35,4 +36,24 @@ public class WebSocketFacade extends Endpoint {
     }
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {}
+
+    /* TODO:
+    *   -CONNECT endpoint
+    *   -MAKE_MOVE endpoint
+    *   -LEAVE endpoint
+    *   -RESIGN endpoint
+    * */
+
+    public void connect(String authToken, int gameID) throws ResponseException{
+        sendCommand(new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID));
+    }
+
+    public void sendCommand(UserGameCommand command) throws ResponseException {
+        try {
+            var json = new Gson().toJson(command);
+            session.getBasicRemote().sendText(json);
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
 }
